6181a369ca36dba3ccd23ea5931cb8840a9e8d0b
mxm_company <maxianming@hikvision.com>
2017-02-07 19:02:03 +0800

    add files

---
+++ src/main/java/com/mxm/learn/collection/Main.java
@@ -0,0 +1,23 @@
+package com.mxm.learn.collection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * <p></p>
+ * @author maxianming 2017年2月6日 下午3:51:46
+ * @version V1.0
+ */
+public class Main {
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		Queue<String> queue = new LinkedList<String>();
+		List<String> list = new LinkedList<String>();
+		Set<String> set = new HashSet<String>();
+	}
+}
---
+++ src/main/java/com/mxm/learn/thread/Main.java
@@ -0,0 +1,18 @@
+package com.mxm.learn.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
+/**
+ * <p>线程入口</p>
+ * @author maxianming 2017年2月6日 上午10:25:03
+ * @version V1.0
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		ExecutorService cachedThreadPool = Executors.newCachedThreadPool(); 
+	}
+}
