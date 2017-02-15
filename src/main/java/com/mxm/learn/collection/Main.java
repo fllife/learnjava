274c74d06e6ceefb333786f12b091662ba3c5f8e
mxm_company <maxianming@hikvision.com>
2017-02-15 18:59:18 +0800

    add files

--- src/main/java/com/mxm/learn/collection/Main.java
+++ src/main/java/com/mxm/learn/collection/Main.java
@@ -2,11 +2,14 @@ package com.mxm.learn.collection;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 /**
  * <p></p>
@@ -16,6 +19,18 @@ import java.util.Set;
 public class Main {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
+		map.put("123", "abc");
+		Set<Map.Entry<String, String>> keyValues = map.entrySet();
+		for (Map.Entry<String, String> entry : keyValues) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+		
+		Map<String, String> coccurentHashMap = new ConcurrentHashMap<String, String>();
+		coccurentHashMap.put("123", "abc");
+		Iterator<String> iter = coccurentHashMap.keySet().iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
 		Queue<String> queue = new LinkedList<String>();
 		List<String> list = new LinkedList<String>();
 		Set<String> set = new HashSet<String>();
